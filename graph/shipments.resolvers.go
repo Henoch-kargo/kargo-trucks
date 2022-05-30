a26229be8063be61aec7e342badb8dbc1277c8f6
Kargo Technologies <henoch.chendra@kargo.tech>
2022-05-30 17:22:21 +0700

    golang with graphql first project

---
+++ graph/shipments.resolvers.go
@@ -0,0 +1,36 @@
+package graph
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"strconv"
+
+	"github.com/Henoch-kargo/kargo-trucks/graph/model"
+)
+
+func (r *mutationResolver) SaveShipment(ctx context.Context, id *string, name string, origin string, destination string, deliveryDate string, truckID string) (*model.Shipment, error) {
+	intTruckId, err := strconv.Atoi(truckID)
+	intId, err := strconv.Atoi(id)
+	if _, ok := r.Trucks[intTruckId]; !ok {
+		return nil, errors.New("TRUCK_UNAVAILABLE")
+	}
+
+	shipment := &model.Shipment{
+		ID:           fmt.Sprintf("Shipment-%d", len(r.Shipments)+1),
+		Name:         name,
+		Origin:       origin,
+		Destination:  destination,
+		DeliveryDate: deliveryDate,
+		TruckID:      truckID,
+	}
+	r.Shipments[intId] = shipment
+	return shipment, nil
+}
+
+func (r *queryResolver) PaginatedShipments(ctx context.Context, id *string, origin *string, page int, first int) ([]*model.Shipment, error) {
+	panic(fmt.Errorf("not implemented"))
+}
